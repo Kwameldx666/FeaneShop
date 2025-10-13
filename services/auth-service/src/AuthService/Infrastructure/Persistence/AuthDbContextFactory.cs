8b95ce384b144ee87fe7a72499b280c24da23c06
asologan <asologan@ENDAUTOXASEBTAS>
2025-10-13 21:17:47 +0300

    Update packages and add User migration
    Updated package versions in AuthService.csproj, including JwtBearer and Swashbuckle. Modified appsettings.json to change the database connection string to use localhost. Added AuthDbContextFactory for design-time DbContext creation. Introduced a new migration to create a Users table with various fields and indexes. Added auto-generated files for migration and model snapshot.

---
+++ services/auth-service/src/AuthService/Infrastructure/Persistence/AuthDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AuthService.Infrastructure.Persistence
+{
+    public class AuthDbContextFactory : IDesignTimeDbContextFactory<AuthDbContext>
+    {
+        public AuthDbContext CreateDbContext(string[] args)
+        {
+            // Загружаем конфигурацию (используем appsettings.json из корня проекта)
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<AuthDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new AuthDbContext(builder.Options);
+        }
+    }
+}
